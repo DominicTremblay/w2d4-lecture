e6114ec15438857442e6a12eef1819a1e27831f2
Dominic Tremblay <dominictremblay24@gmail.com>
2020-06-04 03:22:34 -0400

    reset demo

--- 01_get_user_basic.js
+++ 01_get_user_basic.js
@@ -1,17 +1,9 @@
-const faker = require('faker')
+const faker = require('faker');
 
-const getUser = (cb) => {
-
-  const user = {
-    userId: faker.random.uuid().substring(0,6),
-    avatar: faker.image.avatar(),
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    catchPhrase: faker.company.catchPhrase(),
-  }
-
-cb(user);
-  
-}
-
-getUser(user => console.log(user));
+const user = {
+  userId: faker.random.uuid().substring(0, 6),
+  avatar: faker.image.avatar(),
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  catchPhrase: faker.company.catchPhrase(),
+};
--- 03_get_user_async.js
+++ 03_get_user_async.js
@@ -1,30 +1,26 @@
 const faker = require('faker');
 
 const getUser = (cb) => {
+  // let's add a setTimeout
 
-  setTimeout(() => {
+  const user = {
+    userId: faker.random.uuid().substring(0, 6),
+    avatar: faker.image.avatar(),
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    catchPhrase: faker.company.catchPhrase(),
+  };
 
-    const user = {
-      userId: faker.random.uuid().substring(0, 6),
-      avatar: faker.image.avatar(),
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      catchPhrase: faker.company.catchPhrase(),
-    };
-  
-    throw new Error('Could not get the user');
-  
-    cb(user);
+  // What happens if there is an error?
+  throw new Error('Could not get the user');
 
-  }, 1000);
-
-  
+  cb(user);
 };
 
 try {
   getUser((user) => console.log(user));
 } catch (err) {
-  console.log(err);
+  console.log(err.message);
 }
 
-console.log('Continue Execution...');
+console.log('Continuing Execution...');
