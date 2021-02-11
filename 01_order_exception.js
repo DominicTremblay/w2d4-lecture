a051423d8a6a629b7a32a12078135b5825705e10
Dominic Tremblay <dominictremblay24@gmail.com>
2021-02-10 23:02:20 -0500

    new demo reset

---
+++ 01_order_exception.js
@@ -0,0 +1,5 @@
+const processOrder = (customer) => {};
+
+processOrder('Sponge Bob');
+
+console.log('Sponge Bob waits for his burger...');
---
+++ 05_order_promise.js
@@ -0,0 +1,7 @@
+const executorFct = (resolve, reject) => {
+  const error = false;
+};
+
+const processOrder = (customer) => {
+  console.log(`${customer} orders a burger!`);
+};
